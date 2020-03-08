fd659fa8ba7ee61a45c399e3b85d915aba46e49b
Marius Munthe-Kaas <marius.munthe-kaas@statkraft.com>
2020-03-08 22:20:52 +0100

    Add dockerignore. Add test http-call

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from flask import Flask, request, make_response
 from firebase_admin import credentials, messaging
 import firebase_admin
 import os
-
 import logging
 
 cred = credentials.Certificate('secure/private-api-key.json')
 firebase_admin.initialize_app(credential=cred)
 
 # We want to clear out the logs
-os.remove("logs/log.txt")
+log_name = "logs/log.txt"
+if os.path.exists(log_name):
+    os.remove(log_name)
 
 logging.basicConfig(filename="logs/log.txt", level=logging.DEBUG, format="%(asctime)s;%(levelname)s;%(message)s")
 
@@ -71,6 +72,10 @@ def notification():
         logging.exception(e)
         return res
 
+@app.route("/test", methods=["GET"])
+def test():
+    return "Ok"
+    
 app.run(debug=True, host='0.0.0.0')
 
 
